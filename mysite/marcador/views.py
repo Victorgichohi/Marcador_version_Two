45b35ebad57e743b667dcad8cbc5be6f98976ce1
victorgichohi <vitorgichohi48@gmail.com>
2015-10-06 14:31:46 +0300

    added a function to return bookmarks for a particular user

--- mysite/marcador/views.py
+++ mysite/marcador/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # displays public bookmarks
@@ -10,3 +11,12 @@ def bookmark_list(request):
     bookmarks = Bookmark.public.all()
     context = {'bookmarks': bookmarks}#bookmarks are stored in a dictionary array
     return render(request, 'marcador/bookmark_list.html', context)
+#this gets views bookmarks for a particular user
+def bookmark_user(request, username):
+    user = get_object_or_404(User, username=username)#if user is not found,404 is returned
+    if request.user == user:
+        bookmarks = user.bookmarks.all()
+    else:
+        bookmarks = Bookmark.public.filter(owner__username=username)
+    context = {'bookmarks': bookmarks, 'owner': user}
+    return render(request, 'marcador/bookmark_user.html', context)
