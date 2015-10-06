74edfab91eb122efd097ec6da56bc092e8303f58
victorgichohi <vitorgichohi48@gmail.com>
2015-10-06 14:26:09 +0300

    input a view method in the view.py file to display all public bookmarks

--- mysite/marcador/views.py
+++ mysite/marcador/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+# displays public bookmarks
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Bookmark
+
+
+def bookmark_list(request):
+    bookmarks = Bookmark.public.all()
+    context = {'bookmarks': bookmarks}#bookmarks are stored in a dictionary array
+    return render(request, 'marcador/bookmark_list.html', context)
