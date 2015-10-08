44d03d1ff085d0471296074e36ecf52459dbcd02
victorgichohi <victorgichohi48@gmail.com>
2015-10-08 19:20:51 +0300

    added the link tag for the forms

--- mysite/marcador/urls.py
+++ mysite/marcador/urls.py
@@ -4,5 +4,13 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^user/(?P<username>[-\w]+)/$', 'marcador.views.bookmark_user',
         name='marcador_bookmark_user'),
+        #The pattern ^edit/(?<pk>\d+)/$
+        #has a variable part which catches the primary key (PK) of a bookmark.
+        #The PK is added to every model by Django, so every entry gets a unique identifier.
+        # The path /edit/1/ for example leads to the bookmark with the PK 1.
+         url(r'^create/$', 'marcador.views.bookmark_create',
+        name='marcador_bookmark_create'),
+    url(r'^edit/(?P<pk>\d+)/$', 'marcador.views.bookmark_edit',
+        name='marcador_bookmark_edit'),
     url(r'^$', 'marcador.views.bookmark_list', name='marcador_bookmark_list'),
 ]
