c22b90e6f40e3420bbb6edea69704114a8cf09f7
victorgichohi <victorgichohi48@gmail.com>
2015-10-08 19:40:13 +0300

    added functions in views.py which help in creating new views

--- mysite/marcador/views.py
+++ mysite/marcador/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # displays public bookmarks
 from django.shortcuts import get_object_or_404, redirect, render
-
+from .forms import BookmarkForm
 from .models import Bookmark
 
 
@@ -20,3 +21,20 @@ def bookmark_user(request, username):
         bookmarks = Bookmark.public.filter(owner__username=username)
     context = {'bookmarks': bookmarks, 'owner': user}
     return render(request, 'marcador/bookmark_user.html', context)
+
+#The decorator @login_required makes sure that the view is only accessible by authenticated users. 
+@login_required
+def bookmark_create(request):
+    if request.method == 'POST':
+        form = BookmarkForm(data=request.POST)
+        if form.is_valid():
+            bookmark = form.save(commit=False)
+            bookmark.owner = request.user
+            bookmark.save()
+            form.save_m2m()
+            return redirect('marcador_bookmark_user',
+                username=request.user.username)
+    else:
+        form = BookmarkForm()
+    context = {'form': form, 'create': True}
+    return render(request, 'marcador/form.html', context)
